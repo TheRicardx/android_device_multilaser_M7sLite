c1173250691a195367d9592cd7d34254da40697a
TheRicardx <oliveiraaraujoricardo23@gmail.com>
2026-05-24 19:33:37 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_ML-SO06-M7sLite.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_ML-SO06-M7sLite-user \
-    omni_ML-SO06-M7sLite-userdebug \
-    omni_ML-SO06-M7sLite-eng
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_ML_SO06_M7sLite.mk
